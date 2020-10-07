50d8deef90d17d0148c191adc6899b4de96a386e
Blaine Gomez <BlaineCGomez@outlook.com>
2020-10-06 19:44:24 -0500

    Finished Login

---
+++ ClassesLibrary/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Login
+    {
+        //fields
+        private string _userName;
+        private string _password;
+
+        //properties
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }//end UserName
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }//end Password
+
+        //constructors
+        public Login(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }//end fully qualified constructor
+        public Login() { }//end default constructor
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format("userName-{0}\n" +
+                "password-{1}\n", UserName, Password);
+        }
+
+    }
+}
---
+++ TesterProgram/LoginTester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+namespace TesterProgram
+{
+    class LoginTester
+    {
+        static void Main(string[] args)
+        {
+            string userName = "SpongeBob";
+            string password = "pinapple";
+
+            Login attempt1 = new Login(userName, password);
+            Console.WriteLine(attempt1);
+
+            Login attempt2 = new Login(userName, password);
+            Console.WriteLine(attempt2);
+
+        }
+    }
+}
